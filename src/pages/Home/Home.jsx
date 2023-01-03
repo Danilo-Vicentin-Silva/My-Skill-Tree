98beb3535f023ce6675a43a33802f6b61b7279bf
Danilo <danilovicentindasilva@gmail.com>
2023-01-03 10:39:09 -0300

    Corrigindo raiz do projetos para React Routes

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Header from "/src/components/Header/Header"
+import Sidebar from "/src/components/Sidebar/Sidebar"
+import Main from "/src/components/Main/Main"
+
+const Home = () => {
+	return (
+		<div>
+            <Header />
+            <Sidebar />
+            <Main />
+		</div>
+	)
+}
+
+export default Home
---
+++ src/pages/Projetos/Projetos.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Projetos = () => {
+	return (
+		<div>
+			<h1>Projetos</h1>
+		</div>
+	)
+}
+
+export default Projetos
